0a86e8e6f01d26f8587bc7e130f426a2a735b3ca
LuoxuanLove <LuoxuanLove@users.noreply.github.com>
2026-06-10 21:33:25 +0800

    refactor(filesystem): remove root monolith

--- tests/godot_plugin_harness_fixture/tests/tool_localization_inventory_contract_test.gd
+++ tests/godot_plugin_harness_fixture/tests/tool_localization_inventory_contract_test.gd
@@ -27,6 +27,8 @@ const REMOVED_PUBLIC_TOOL_LOCALIZATION_KEYS: Array[String] = [
 	"tool_system_editor_log_desc",
 	"tool_resource_manage_name",
 	"tool_resource_manage_desc",
+	"tool_filesystem_file_name",
+	"tool_filesystem_file_desc",
 	"tool_debug_log_name",
 	"tool_debug_log_desc"
 ]
